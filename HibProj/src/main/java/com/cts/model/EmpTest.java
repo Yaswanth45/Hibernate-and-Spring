458b0d8038d77bc424e01138bad907801bccc9ed
T.K. Yaswanth Reddy <52193125+Yaswanth45@users.noreply.github.com>
2020-02-04 03:00:27 -0800

    Add files via upload
    Hibernate Project

---
+++ HibProj/src/main/java/com/cts/model/EmpTest.java
@@ -0,0 +1,23 @@
+package com.cts.model;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class EmpTest {
+
+	public static void main(String[] args) {
+		EntityManagerFactory emf= Persistence.createEntityManagerFactory("testdb");
+		EntityManager em= emf.createEntityManager();
+		em.getTransaction().begin();
+		Emp e= new Emp();
+		e.setEname("Yaswanth");
+		em.persist(e);
+		em.getTransaction().commit();
+		em.close();
+		emf.close();
+		System.out.println("Successfull...................");
+
+	}
+
+}
